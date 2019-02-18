d32945f7a75c651b45aeaa9e9b006cade8d32680
Dong Yu Yu <scratch19@gmail.com>
2019-02-19 06:10:52 +0800

    regular expression

--- app/src/main/java/uga/cs/newsapplication/MainActivity.java
+++ app/src/main/java/uga/cs/newsapplication/MainActivity.java
@@ -1,20 +1,80 @@
 package uga.cs.newsapplication;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements HeadlinesFragment.OnHeadlineSelectedListener{
+    EditText editText;
 
+
+
+
+
+
+
+
+    Button button;
+    Pattern p ;
+    public boolean validate(String s) {
+
+
+
+
+
+
+        if (Patterns.EMAIL_ADDRESS.matcher(s).matches()) {
+            return true;
+        }else {
+            return false;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        editText = findViewById(R.id.editText);
+
+        button = findViewById(R.id.button);
+
+
+
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+
+
+                String s = editText.getText().toString();
+
+                if (!validate(s)) {
+                    Toast.makeText(MainActivity.this, "please enter valid.", Toast.LENGTH_LONG).show();
+                };
+            }
+        });
     }
     @Override
     public void onArticleSelected(int position,String item){
         NewsarticaleFragment articleFrag = (NewsarticaleFragment)
-                getSupportFragmentManager().findFragmentById(R.id.newsArticale);
+                getSupportFragmentManager().findFragmentById(R.id.newsContainer);
 
 
 
@@ -32,9 +92,12 @@ public class MainActivity extends AppCompatActivity implements HeadlinesFragment
 
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
-            // Replace whatever is in the fragment_container view with this fragment,
             // and add the transaction to the back stack so the user can navigate back
-            transaction.replace(R.id.container, newFragment);
+    //        Fragment f = getSupportFragmentManager().findFragmentById(R.id.headlinefragment);
+    //        transaction.remove(f);
+            transaction.replace(R.id.headlinefragment, newFragment);
+
+
             transaction.addToBackStack(null);
 
             // Commit the transaction
